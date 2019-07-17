58827bec7018eec00536478798cddc40a0c14b40
ltj <a121bc@163.com>
2019-07-17 10:22:10 +0800

    Initial commit

---
+++ src/main/java/com/ltj/webmagic/PageProcessor/GithubRepoPageProcessor.java
@@ -0,0 +1,41 @@
+package com.ltj.webmagic.PageProcessor;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * 描 述 爬虫1
+ * 创 建 人 刘天珺
+ * 创建时间 2019-7-17 0017 09:02
+ */
+public class GithubRepoPageProcessor implements PageProcessor {
+
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
+
+    @Override
+    public void process(Page page) {
+        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
+        page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+).*").toString());
+        page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
+        if (page.getResultItems().get("name")==null) {
+            page.setSkip(true);
+        }
+        page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new GithubRepoPageProcessor())
+                .setDownloader(new HttpClientDownloader())
+                .addUrl("https://github.com/code4craft")
+                .addPipeline(new JsonFilePipeline("D:\\webmagic\\"))
+                .thread(5).run();
+    }
+}
